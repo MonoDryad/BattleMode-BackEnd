66bae167baaeb4ab9b375f3963d1bf0342e6498e
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-09-27 14:58:26 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,26 @@ const app = require('./src/app')
 
 const PORT = process.env.PORT || 3000
 
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}/api `)
-}) 
+})
