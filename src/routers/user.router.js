bf7d61f1a2cc58f96659d8c63a09e5e756bd6882
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-10-20 11:20:00 -0300

    Update user.router.js

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -2,7 +2,7 @@ const router = require('express-promise-router')()
 const userController = require('../controllers/user.controllers')
 
 router.get('/user', userController.findAll)
-router.get('/user/:id', userController.findById)
+router.get('/user/:id', userController.findByPk)
 router.post('/user', userController.create)
 router.put('/user/:id', userController.update)
 router.delete('/user/:id', userController.delete)
